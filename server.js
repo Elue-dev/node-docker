e6ff7da7ad7afb6b5da8554cdb59c7ffe937eefe
Wisdom <eluewisdom@gmail.com>
2023-09-24 18:37:20 +0100

    patch: modify docker compose file to specify node app service depends on db service

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+(function connectWithRetry() {
+  mongoose
+    .connect(
+      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+    )
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => {
+      console.log("MongoDB Connection Failed:", error);
+      setTimeout(() => connectWithRetry, 5000);
+    });
+})();
 
 app.get("/", function (req, res) {
   res.json("Hello from docker...");

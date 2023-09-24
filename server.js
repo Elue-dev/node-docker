64b612b92cf44b2c9e666bba614a05135a0557c6
Wisdom <eluewisdom@gmail.com>
2023-09-24 17:45:04 +0100

    feat: add named volume for mongodb to docker compose and add mongoose library

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-  res.json("Hello from docker");
+  res.json("Hello from docker...");
 });
 
 app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
